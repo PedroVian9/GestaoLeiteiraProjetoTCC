608e0643f0437737cfa0b935a22e5f32f0b247b8
Pedro Macedo Paronetto Faria Viana <pmpviana@gmail.com>
2025-03-07 14:18:02 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoLeiteiraProjetoTCC/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GestaoLeiteiraProjetoTCC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
