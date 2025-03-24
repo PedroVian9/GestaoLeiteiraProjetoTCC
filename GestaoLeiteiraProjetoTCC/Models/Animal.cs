744ba5af2e545739f61633405ee1dab3529f4da1
Pedro Macedo Paronetto Faria Viana <pmpviana@gmail.com>
2025-03-24 18:04:51 -0300

    Fix: Ajustes para criação de DB e finalizacao de refinamentos para começar a trabalhar no crud animais

--- GestaoLeiteiraProjetoTCC/Models/Animal.cs
+++ GestaoLeiteiraProjetoTCC/Models/Animal.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaoLeiteiraProjetoTCC.Models
 {
-    internal class Animal
+    public class Animal
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A espécie é obrigatória.")]
+        public string Especie { get; set; }
+
+        public string? Raca { get; set; }
+
+        [Required(ErrorMessage = "O nome identificador é obrigatório.")]
+        public string NomeIdentificador { get; set; }
+
+        public DateTime? DataNascimento { get; set; }
+
+        public string? EstadoFisiologico { get; set; } // Lactante, Seca, Prenha
+
+        public int? MaeId { get; set; }
+        public int? PaiId { get; set; }
+
+        [ForeignKey("MaeId")]
+        public Animal? Mae { get; set; }
+
+        [ForeignKey("PaiId")]
+        public Animal? Pai { get; set; }
+
+        [Required(ErrorMessage = "O id da propriedade é obrigatório.")]
+        public int PropriedadeId { get; set; }
+
+        [ForeignKey("PropriedadeId")]
+        public Propriedade Propriedade { get; set; }
     }
 }
